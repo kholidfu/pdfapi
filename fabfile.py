f2627b66019de8aeb7bde7d890bd9b81fe13ef5c
sopier <sopier@gmail.com>
2014-02-16 00:32:12 +0700

    asdasd

--- fabfile.py
+++ fabfile.py
@@ -4,12 +4,11 @@
 Automate flask app deployment
 
 What you need to run this:
-1. zipped app/ run.py and uwsgi.ini
-2. default file which contain nginx conf
+1. zipped app/ run.py and uwsgi.ini => format domain.xxx.tar.gz
+2. "default" file which contain all sites conf
 3. id_rsa.pub to connect to server without password prompt
+4. supervisord.conf latest version
 
-todo:
-1. change server_names_hash_bucket_size 64 di /etc/nginx/nginx.conf :: not yet
 """
 
 # fabric thing
@@ -19,6 +18,7 @@ from farmers import Farmers
 
 f = Farmers()
 env.hosts = [f.droplet_ip()]
+droplet_ip = env.hosts[0]
 
 def add_domain(site):
     """ adding domain to DO"""
@@ -33,7 +33,7 @@ def create_key():
     """ delete old keys and generate new one"""
     local("> ~/.ssh/known_hosts")
     local("ssh-copy-id -i /home/banteng/.ssh/id_rsa.pub sopier@" \
-          + f.droplet_ip())
+          + droplet_ip)
 
 def install_packages():
     env.user = "sopier"
@@ -60,7 +60,7 @@ def upload_package(package, domain):
     """upload folder app/ run.py and uwsgi.ini from localhost"""
     env.user = "sopier"
     env.key_filename = "/home/banteng/.ssh/id_rsa"
-    local("scp " + package + " sopier@" + f.droplet_ip() + ":")
+    local("scp " + package + " sopier@" + droplet_ip + ":")
 
     run("mv " + package + " " + domain + "/")
     run("cd " + domain + " && tar zxvf " + package)
@@ -73,7 +73,7 @@ def setup_nginx():
     """
     env.user = "root"
     env.key_filename = "/home/banteng/.ssh/id_rsa"
-    local("scp default root@" + f.droplet_ip() \
+    local("scp default root@" + droplet_ip \
           + ":/etc/nginx/sites-available/default")
     sudo("sed -i 's/.*64.*/server_names_hash_bucket_size 64;/' /etc/nginx/nginx.conf")
     sudo("/etc/init.d/nginx restart")
@@ -82,18 +82,18 @@ def set_supervisor(domain):
     """ setup for supervisor """
     env.user = "sopier"
     env.key_filename = "/home/banteng/.ssh/id_rsa"
-    local("scp run.py sopier@" + f.droplet_ip() + ":/home/sopier/" + domain)
-    local("scp supervisord.conf sopier@" + f.droplet_ip() + ":")
+    local("scp run.py sopier@" + droplet_ip + ":/home/sopier/" + domain)
+    local("scp supervisord.conf sopier@" + droplet_ip + ":")
 
 def run_site():
     """ run the site """
     env.user = "sopier"
     env.key_filename = "/home/banteng/.ssh/id_rsa"
     try:
-        run("sudo pkill supervisord")
+        sudo("pkill supervisord")
     except:
         pass
-    run("sudo supervisord -c supervisord.conf")
+    sudo("supervisord -c supervisord.conf")
 
 def setup_server():
     create_user()
