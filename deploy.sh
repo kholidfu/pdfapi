01d7e48bfdce0d395ca371e02d1b437574649c78
sopier <sopier@gmail.com>
2014-02-15 09:15:07 +0700

    adding argument

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
-fab create_user:"sopier"
+fab create_user:$1
 fab create_key
 fab install_packages
-fab create_venv:"hotoid.com"
-fab install_packages_venv:"hotoid.com"
-fab upload_package:"hotoid.com.tar.gz","hotoid.com"
+fab create_venv:$2
+fab install_packages_venv:$2
+fab upload_package:$3,$2
 fab setup_nginx
-fab run_the_site:"hotoid.com"
+fab run_the_site:$2
